ef3c690f27acee778f864aea66b5150fe189baad
ms827 <minseok.tech@gmail.com>
2025-05-04 01:15:50 +0900

    feat : 약관동의 페이지

--- app/auth/terms/page.tsx
+++ app/auth/terms/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useState } from "react";
-import { X, ChevronRight } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { useRouter } from "next/navigation";
+import AuthHeader from "@/app/auth/_components/auth-header";
 
 const termsList = [
   { id: 1, label: "서비스 이용약관 동의", required: true },
@@ -32,54 +33,57 @@ export default function TermsPage() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white">
-      <div className="w-full p-6 rounded-xl shadow-md border border-gray-100 relative">
-        {/* 닫기 버튼 */}
-        <button className="absolute right-4 top-4 text-2xl text-gray-400 hover:text-gray-600">
-          <X size={24} />
-        </button>
-        {/* 약관 전체 동의 */}
-        <div className="flex items-center gap-2 mb-6 mt-2">
-          <Checkbox
-            checked={allChecked}
-            onCheckedChange={handleAll}
-            className="rounded-full bg-gray-4"
-            aria-label="약관 전체 동의"
-          />
-          <span className="font-semibold text-lg text-gray-700">
-            약관에 모두 동의
-          </span>
-        </div>
-        {/* 개별 약관 */}
-        <div className="flex flex-col gap-4 mb-8">
-          {termsList.map((term, idx) => (
-            <div key={term.id} className="flex items-center gap-2">
-              <Checkbox
-                checked={checked[idx]}
-                onCheckedChange={() => handleCheck(idx)}
-                className=" bg-gray-4"
-                aria-label={term.label}
-              />
-              <span className="text-gray-500 text-base">{term.label}</span>
-              <span
-                className="ml-auto text-gray-300 text-xl cursor-pointer"
-                onClick={() => handleDetail(idx)}
-              >
-                <ChevronRight size={24} className="cursor-pointer" />
-              </span>
-            </div>
-          ))}
-        </div>
-        {/* 확인 버튼 */}
-        <button
-          className={`w-full h-12 rounded-md text-white text-lg font-medium ${
-            allChecked ? "bg-primary-black" : "bg-gray-200"
-          }`}
-          disabled={!allChecked}
-        >
-          확인
-        </button>
+    <div className="flex flex-col h-screen">
+      <div className="flex-none py-4">
+        <AuthHeader title="약관동의" />
       </div>
-    </main>
+      <div className="flex-1 flex flex-col justify-end">
+        {/* 약관 동의 내역 */}
+        <div className="overflow-y-auto p-4 text-sm rounded-t-xl border border-b-0 border-gray-200">
+          {/* 약관 전체 동의 */}
+          <div className="flex items-center gap-4">
+            <Checkbox
+              checked={allChecked}
+              onCheckedChange={handleAll}
+              className="rounded-full bg-gray-4"
+              aria-label="약관 전체 동의"
+            />
+            <span className="text-lg text-[#1C1C1C]">약관에 모두 동의</span>
+          </div>
+          <div className="border-t border-gray-200 my-5"></div>
+          {/* 개별 약관 */}
+          <div className="flex flex-col gap-4">
+            {termsList.map((term, idx) => (
+              <div key={term.id} className="flex items-center gap-4">
+                <Checkbox
+                  checked={checked[idx]}
+                  onCheckedChange={() => handleCheck(idx)}
+                  className="bg-gray-4"
+                  aria-label={term.label}
+                />
+                <span className="text-gray-500 text-lg">{term.label}</span>
+                <span
+                  className="ml-auto text-gray-300 cursor-pointer"
+                  onClick={() => handleDetail(idx)}
+                >
+                  <ChevronRight size={20} className="cursor-pointer" />
+                </span>
+              </div>
+            ))}
+          </div>
+          {/* 확인 버튼 */}
+          <button
+            className={`w-full h-12 text-lg rounded-md font-medium mt-8 ${
+              allChecked
+                ? "bg-primary-black text-white"
+                : "bg-gray-4 text-gray-2"
+            }`}
+            disabled={!allChecked}
+          >
+            확인
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
